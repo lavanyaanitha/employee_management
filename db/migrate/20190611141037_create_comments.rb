26503b14f0352e5ea00ef3d3aac0cf36835f071e
lavanyaanitha <lavanyasubramani18@gmail.com>
2019-06-18 08:19:02 +0530

    sqlite version mentioned

--- db/migrate/20190611141037_create_comments.rb
+++ db/migrate/20190611141037_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.string :username
       t.text :body
-      t.references :post, foreign_key: true
 
       t.timestamps
     end
